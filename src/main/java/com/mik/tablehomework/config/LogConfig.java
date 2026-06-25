7ef73d8c7770669cd4fc76e3bf93e479b3c7cbc1
mikAkhmetov <1msmcomp2jj@gmail.com>
2026-06-25 14:55:31 +0300

    modify log config

--- src/main/java/com/mik/tablehomework/config/LogConfig.java
+++ src/main/java/com/mik/tablehomework/config/LogConfig.java
@@ -1,4 +1,28 @@
 package com.mik.tablehomework.config;
 
+import java.util.ResourceBundle;
+
 public class LogConfig {
+
+    private static System.Logger logger = new System.Logger() {
+        @Override
+        public String getName() {
+            return "";
+        }
+
+        @Override
+        public boolean isLoggable(Level level) {
+            return false;
+        }
+
+        @Override
+        public void log(Level level, ResourceBundle resourceBundle, String s, Throwable throwable) {
+
+        }
+
+        @Override
+        public void log(Level level, ResourceBundle resourceBundle, String s, Object... objects) {
+
+        }
+    };
 }
